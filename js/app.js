59961a4053622e5fddc3c431a7e82ab5a160643a
Hamza Qahoush <haamza.qah@gmail.com>
2021-03-03 15:59:28 +0200

    I used another code for arrays and its easier and work prefectly

--- js/app.js
+++ js/app.js
@@ -1,115 +1,137 @@
-'use strict'
+    'use strict';
 
-let score = 0 ;
+    let score = 0 ;
 
-let wlc = prompt('Welcome To Guess Game.. Please answer Yes or No , Type Your Name  :) ')
-alert ('Hello'+  ' ' + wlc + ' '+ 'The Game will Start')
+    let wlc = prompt('Welcome To Guess Game.. Please answer Yes or No , Type Your Name  :) ')
+    alert ('Hello'+  ' ' + wlc + ' '+ 'The Game will Start')
 
-let jump = prompt('Can cats jump six times their length ?') ;
-if (jump === 'YES' || jump ==='yes' || jump ==='y' || jump ==='Y' || jump ==='Yes'){
-    alert ('Your answer is corect')
-    score++;
-}else {
-    alert ( 'Your answer is wrong')
-}
-
-let cheese = prompt('Can cats eat cheese ?');
-if (cheese === 'YES' || cheese === 'yes' || cheese === 'y' || cheese === 'Y' || cheese === 'Yes') {
-    alert ('Your answer is corect')
-    score++; 
-}else {
-    alert ( 'Your answer is wrong')
-;}
-let swim = prompt('Can cats swim in water ?');
-if (swim === 'YES' || swim ==='yes' || swim ==='y' || swim ==='Y' || swim ==='Yes'){
-    alert ('Your answer is corect')
-    score++;
-}else if (swim ==='No' ||swim === 'no' || swim ==='n' || swim ==='N'){
-    alert ( 'Your answer is wrong ')
-} 
-
-let forest = prompt('Can cats live in forests?');
-if (forest === 'No' || forest ==='no' || forest ==='n' || forest ==='N' ){
-    alert ('Your answer is corect')
-    score++;
-}else {
-    alert ( 'Your answer is wrong')
-}
-let grass = prompt('Do cats eat grass?');
-if (grass === 'YES' || grass ==='yes' || grass ==='y' || grass ==='Y' || grass ==='Yes' ){
-    alert ('Your answer is corect' + ' ' + 'cat may graze on grass to boost its vitamin levels.')
-    score++;
-}else {
-    alert ( 'Your answer is wrong'+ ' ' +'cat may graze on grass to boost its vitamin levels.') 
-}
-
-alert ('Thanks You For Joining My Guess Cat Game '+  ' ' + wlc + ' ' ) 
-
-document.write ("Hello " + wlc + "! How are you today? , Did You like this Game :D");
-console.log(jump); console.log(cheese); console.log(swim) ; console.log(forest); console.log(grass);
-
-
-//here is the 6th question
-
-let Myage= 29 ;
-let counter=0 ;
- for (let i=0 ; i<4 ; i++){
-    let guessage = prompt( 'try to guess my age , You have 4 attempts') ;
-    let newAge = parseInt(guessage) ;
-    if (newAge<=20 || newAge >= 30){
-        alert('You are  far ..try again');
-    }
-    else if (newAge >=21 && newAge <=25){
-    alert('You are little far try again!') ;
-    }
-    else if (newAge>=26 && newAge<=28){
-        alert('You are close but try again' ) ;
-    }
-    else if (newAge===29){
-        alert('You are Correct!');
+    let jump = prompt('Can cats jump six times their length ?') ;
+    if (jump === 'YES' || jump ==='yes' || jump ==='y' || jump ==='Y' || jump ==='Yes'){
+        alert ('Your answer is corect')
         score++;
-        break ;
+    }else {
+        alert ( 'Your answer is wrong')
     }
-    counter =+i
-}
-console.log (counter ) ;
-if  (counter===3){
 
-    alert('The correct answer is 29')
+    let cheese = prompt('Can cats eat cheese ?');
+    if (cheese === 'YES' || cheese === 'yes' || cheese === 'y' || cheese === 'Y' || cheese === 'Yes') {
+        alert ('Your answer is corect')
+        score++; 
+    }else {
+        alert ( 'Your answer is wrong')
+    ;}
+    let swim = prompt('Can cats swim in water ?');
+    if (swim === 'YES' || swim ==='yes' || swim ==='y' || swim ==='Y' || swim ==='Yes'){
+        alert ('Your answer is corect')
+        score++;
+    }else if (swim ==='No' ||swim === 'no' || swim ==='n' || swim ==='N'){
+        alert ( 'Your answer is wrong ')
+    } 
+
+    let forest = prompt('Can cats live in forests?');
+    if (forest === 'No' || forest ==='no' || forest ==='n' || forest ==='N' ){
+        alert ('Your answer is corect')
+        score++;
+    }else {
+        alert ( 'Your answer is wrong')
+    }
+    let grass = prompt('Do cats eat grass?');
+    if (grass === 'YES' || grass ==='yes' || grass ==='y' || grass ==='Y' || grass ==='Yes' ){
+        alert ('Your answer is corect' + ' ' + 'cat may graze on grass to boost its vitamin levels.')
+        score++;
+    }else {
+        alert ( 'Your answer is wrong'+ ' ' +'cat may graze on grass to boost its vitamin levels.') 
+    }
+
+    alert ('Thanks You For Joining My Guess Cat Game '+  ' ' + wlc + ' ' ) 
+
+    document.write ("Hello " + wlc + "! How are you today? , Did You like this Game :D");
+    console.log(jump); console.log(cheese); console.log(swim) ; console.log(forest); console.log(grass);
+
+
+    //here is the 6th question
+
+    let Myage= 29 ;
+    let counter=0 ;
+    for (let i=0 ; i<4 ; i++){
+        let guessage = prompt( 'try to guess my age , You have 4 attempts') ;
+        let newAge = parseInt(guessage) ;
+        if (newAge<=20 || newAge >= 30){
+            alert('You are  far ..try again');
+        }
+        else if (newAge >=21 && newAge <=25){
+        alert('You are little far try again!') ;
+        }
+        else if (newAge>=26 && newAge<=28){
+            alert('You are close but try again' ) ;
+        }
+        else if (newAge===29){
+            alert('You are Correct!');
+            score++;
+            break ;
+        }
+        counter =+i
+    }
+    console.log (counter ) ;
+    if  (counter===3){
+
+        alert('The correct answer is 29')
+        
+    }
+
+    //This is The 7th Question method 2 
+    let Counter=0;
+ let FavColor= ['black' ,'white' , 'blue' , 'grey', 'brown' ,'green'];
+    for(let i = 0; i<FavColor.length; i++){     
+    let question = prompt('Guess My favourite Color??').toLowerCase()
+    if(question === FavColor[i]){
+        alert('You Have Got a Color')
+        score++;
+        break;
+    }else{
+        (alert ('Try again' +' '+' hint :) first letter is w '))
+        Counter=+i
+        }
+    }
+    if (Counter===5){
+        alert('Hard Luck the answers are Black , White , Blue , Grey')
+    }
+   
     
-}
+    alert('Your score out of 7 is' + ' ' + score) ;
+    console.log(FavColor[0]);console.log(FavColor[1]);console.log(FavColor[2]);console.log(FavColor[3]);console.log(Myage);
 
-//This is The 7th Question 
+// method 1 
 
-let Counter=0;
-let FavColor= ['Black' ,'White' , 'Blue' , 'Grey'];
-for(let i = 0; i < 6; i++){     
-let question = prompt('Guess My favourite Color??')
- if(question === FavColor[0].toLowerCase()){
-    alert('You Have Got the Colors')
-    score++;
-     break;
-} else if (question === FavColor[1].toLowerCase()){
-    alert('You Have Got the Colors')
-    score++;
-    break ;
-}
-else if (question === FavColor[2].toLowerCase()){
-    alert('You Have Got the Colors')
-    score++;
-    break ;
-}
-else if (question === FavColor[3].toLowerCase()){
-    alert('You Have Got the Colors')
-    score++;
-    break ;
-}
-else (alert ('Try again' +' '+'My Favourite Colors are '))
-Counter=+i
-}
+    // let Counter=0;
+    // let FavColor= ['Black' ,'White' , 'Blue' , 'Grey'];
+    // for(let i = 0; i < 6; i++){     
+    // let question = prompt('Guess My favourite Color??')
+    //  if(question === FavColor[0].toLowerCase()){
+    //     alert('You Have Got the Colors')
+    //     score++;
+    //      break;
+    // } else if (question === FavColor[1].toLowerCase()){
+    //     alert('You Have Got the Colors')
+    //     score++;
+    //     break ;
+    // }
+    // else if (question === FavColor[2].toLowerCase()){
+    //     alert('You Have Got the Colors')
+    //     score++;
+    //     break ;
+    // }
+    // else if (question === FavColor[3].toLowerCase()){
+    //     alert('You Have Got the Colors')
+    //     score++;
+    //     break ;
+    // }
+    // else (alert ('Try again' +' '+'My Favourite Colors are '))
+    // Counter=+i
+    // }
 
-if (Counter===5){
-    alert('Hard Luck the answers are Black , White , Blue , Grey')
-}
-alert('Your score out of 7 is' + ' ' + score) ;
-console.log(FavColor[0]);console.log(FavColor[1]);console.log(FavColor[2]);console.log(FavColor[3]);console.log(Myage);
+    // if (Counter===5){
+    //     alert('Hard Luck the answers are Black , White , Blue , Grey')
+    // }
+    // alert('Your score out of 7 is' + ' ' + score) ;
+    // console.log(FavColor[0]);console.log(FavColor[1]);console.log(FavColor[2]);console.log(FavColor[3]);console.log(Myage);
